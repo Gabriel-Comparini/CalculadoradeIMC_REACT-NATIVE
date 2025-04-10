6d555a79d86ed0749701a54c1915bd5c79ca7524
Gabriel Comparini <gc77777777777777@gmail.com>
2025-04-10 16:28:55 -0300

    Update comentarios App.js

--- CalculadoraIMC/App.js
+++ CalculadoraIMC/App.js
@@ -1,7 +1,9 @@
+//Comandos para importar (pegar) elementos/funções de outros documentos
 import FormIMC from './src/components/FormIMC';
 import { StyleSheet, View } from 'react-native';
 import Title from './src/components/Title';
 
+//Função padrão do App.js, é a função principal, ela executa o código
 export default function App() {
   return (
     <View style={styles.container}>
@@ -11,6 +13,7 @@ export default function App() {
   );
 }
 
+//Estilização do view do App.js
 const styles = StyleSheet.create({
   container: {
     flex: 1,
