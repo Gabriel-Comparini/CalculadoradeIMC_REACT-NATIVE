e5bedfe3591600bafbc9dbd1932bf638cd2268a2
Gabriel Comparini <gc77777777777777@gmail.com>
2025-04-10 16:50:49 -0300

    Update IdealWeight.js

--- CalculadoraIMC/src/components/IdealWeight.js
+++ CalculadoraIMC/src/components/IdealWeight.js
@@ -1,15 +1,21 @@
+//Aqui são importados do React e React-Native os elementos usados no documento.
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
+//Essa é a função principal do IdealWeight.js
 const IdealWeight = ({ altura }) => {
+    //Esse If/Else define se o valor importado do FormIMC.js é nulo ou não.
     if(altura == null){
         return null;
     } else if (altura) {
+        //Essas são as contas necessária para definir seu peso máximo com base na altura.
+        //A altura é dividida por 100 para ficar em metros.
         altura = altura/100;
         let minWeight = 18.5 * altura * altura;
         let maxWeight = 24.9 * altura * altura;
         
         return (
+            //Elemento impresso pela função.
             <View style = {styles.idealwView}>
                 <Text style = {styles.idealweight}>
                     Seu Peso mínimo ideal é {minWeight.toFixed(2)}Kg
@@ -22,6 +28,7 @@ const IdealWeight = ({ altura }) => {
     
 }
 
+//Estilização do elemento impresso pela função.
 const styles = StyleSheet.create({
 
     idealweight: {
@@ -42,4 +49,5 @@ const styles = StyleSheet.create({
     },
 });
 
+//Exporta a função IdealWeight. 
 export default IdealWeight;
