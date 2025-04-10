b17620b6292b5235607f3bd3b35dcd66b4129882
Gabriel Comparini <gc77777777777777@gmail.com>
2025-04-10 17:09:29 -0300

    Update FormIMC.js

--- CalculadoraIMC/src/components/FormIMC.js
+++ CalculadoraIMC/src/components/FormIMC.js
@@ -1,9 +1,11 @@
+//Comandos para importar (pegar) elementos/funções de outros documentos.
 import { View, TextInput, Button, StyleSheet } from "react-native";
 import { useState } from "react";
 import Result from "./Result";
 import Classification from "./Classification";
 import IdealWeight from "./IdealWeight";
 
+//Função padrão do FormIMC, ela cria imputs para coletar os valores altura e peso, com eles, uma conta é realizada por meio de outra função (interna), para assim calcular o imc.
 const FormIMC = () =>{
     const [peso, setPeso] = useState(0);
     const [altura, setAltura] = useState(0);
@@ -17,6 +19,7 @@ const FormIMC = () =>{
         }
     };
     
+    //Imprime os Imputs e o button.
     return (
         <View style = {styles.formContainer}>
             <TextInput 
@@ -38,12 +41,15 @@ const FormIMC = () =>{
 
             <Button title = "Calcular IMC" onPress = {calcularIMC} style = {styles.resultButton} />
             {imc && <Result imc = {imc} />}
+
+            {/* As duas linhas seguintes exportam os valores do imc e da altura para os outros documentos. */}
             <Classification imc={imc}/>
             <IdealWeight altura = {altura}/>
         </View>
     );
 };
 
+//Estilização dos elementos do documento.
 const styles = StyleSheet.create({
     formContainer: {
         backgroundColor:'#f0f0f0',
@@ -63,4 +69,5 @@ const styles = StyleSheet.create({
 
 });
 
+//Exporta o FormIMC.
 export default FormIMC;
